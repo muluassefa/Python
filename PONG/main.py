da9510723cd17ae0001e88ae7a35c534646ab13e
Kasa Mulu Assefa (DC ATV SP PD PTE TE APS) <MuluAssefa.Kasa@infineon.com>
2024-05-27 10:25:45 +0200

    commit

---
+++ PONG/main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+import Paddle
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=600, height=500)
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle(350, 0)
+l_paddle = Paddle(-350, 0)
+
+
